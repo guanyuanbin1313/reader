816b07034e6b4940082c5555d7d02de4f2b58f40
zcq1933 <1933748107@qq.com>
2020-04-22 19:59:40 +0800

    sqlconnection

--- back/common/sqlConnection.js
+++ back/common/sqlConnection.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 
 
 var connection = mysql.createConnection({
-  host:'127.0.0.1',
-  post:'3306',
-  user:'root',
-  password:'',
-  database:'reader'
+    host: '101.37.172.171',
+    post: '3306',
+    user: 'root',
+    password: '12345',
+    database: 'reader'
 })
 
 module.exports = connection;
