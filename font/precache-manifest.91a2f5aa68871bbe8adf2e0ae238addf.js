7658678492f3b42915d6266951aff4dea6c04a15
zcq1933 <1933748107@qq.com>
2020-04-22 20:44:15 +0800

    upload font end

---
+++ font/precache-manifest.91a2f5aa68871bbe8adf2e0ae238addf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073f22808562a9bcc4efb183f89cc456",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ad5524ce9c07e38e3cd",
+    "url": "/static/css/2.90172c5a.chunk.css"
+  },
+  {
+    "revision": "18324226d2589ae76a55",
+    "url": "/static/css/main.70ba21e6.chunk.css"
+  },
+  {
+    "revision": "7ad5524ce9c07e38e3cd",
+    "url": "/static/js/2.650ce859.chunk.js"
+  },
+  {
+    "revision": "18324226d2589ae76a55",
+    "url": "/static/js/main.bb4b6d31.chunk.js"
+  },
+  {
+    "revision": "b7989f347b280d1dd087",
+    "url": "/static/js/runtime-main.019026b0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
